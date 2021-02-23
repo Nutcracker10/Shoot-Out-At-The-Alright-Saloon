46a107ca34bd2b00f61df824434d41574abac18b
James <jamesgkirwan@gmail.com>
2021-02-23 15:41:07 +0000

    Beginning new room attempt

--- src/util/Door.java
+++ src/util/Door.java
@@ -1,11 +1,13 @@
 package util;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class Door {
+    private int x, y;
     private int height =80, width = 80;
     private boolean isOpen = false;
     private BufferedImage image;
@@ -21,6 +23,19 @@ public class Door {
             image = ImageIO.read(new File("res/Open_door.png"));
         } catch (IOException e) { e.printStackTrace(); }
         isOpen = true;
+    }
+
+    public boolean playerEnters(GameObject Player) {
+        // TODO check if player is colliding with open door, load new room
+        if  (Math.abs(Player.getCentre().getX() - this.x) < Player.getWidth()
+                && Math.abs(Player.getCentre().getY()- this.y) < Player.getHeight() ) {
+            return true;
+        } else
+        return false;
+    }
+
+    public void draw(Graphics g) {
+        g.drawImage(image, x, y, width, height, null);
     }
 
     public void setOpen(boolean isOpen) { this.isOpen = isOpen ;}
